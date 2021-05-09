6d2751ce24a03b2c57170d76da346b6f70f0f287
Rafael Freitas <69989761+Rafaa-Freitas@users.noreply.github.com>
2021-05-09 17:48:45 -0300

    Adição de manipulação de arquivos externos
    Manipulando arquivos externos para exibir mensagem na tela inicial.

--- AP2-Prática_Python/concessionaria.py
+++ AP2-Prática_Python/concessionaria.py
@@ -5,16 +5,19 @@ clientes = []
 motos = []
 
 def __main__():
-    primeira_escolha = int(input('1 - Clientes \n2 - Motos \n3 - Vendas\nDeseja atuar em: '))
+    carrega_mensagem_inicial();
+
+    primeira_escolha = int(input('\n1 - Clientes \n2 - Motos \n3 - Vendas\n4 - Encerrar\n\nO que deseja: '))
+    print('\n')
     if (primeira_escolha == 1):
-        segunda_escolha = int(input('1 - Cadastrar Cliente \n2 - Buscar por Cliente \n3 - Atualizar dados do Cliente \n4 - Deletar Cliente \nDigite o que deseja: '))
+        print(f'*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n'+12*' '+'Clientes\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*')
+        segunda_escolha = int(input('\n1 - Cadastrar Cliente \n2 - Buscar por Cliente \n3 - Atualizar dados do Cliente \n4 - Deletar Cliente\n\nDigite o que deseja: '))
         if(segunda_escolha == 1):
-            nome = input('Digite um nome: ')
-            endereco = input('Digite um endereço: ')
-            telefone = input('Digite um telefone: ')
+            nome = input('\nDigite um nome: ')
+            endereco = input('\nDigite um endereço: ')
+            telefone = input('\nDigite um telefone: ')
             criar_cliente(nome, endereco, telefone, clientes)
-            print(f'Cliente {nome} criado com sucesso!')
-            # print(clientes[0][0])
+            print(f'\nCliente {nome} criado com sucesso!')
 
             print(clientes)
 
@@ -27,13 +30,13 @@ def __main__():
             nome_antigo = input('Digite o nome do cliente que deve ser alterado: ')
             terceira_escolha = int(input('1 - Nome, 2 - Endereço, 3 - Telefone\nO que deseja alterar: '))
             if (terceira_escolha == 1):
-                nome = input('Digite um novo nome: ')
+                nome = input('\nDigite um novo nome: ')
                 alterar_cliente(nome, 0, nome_antigo)
             elif (terceira_escolha == 2):
-                endereco = input('Digite um novo endereço: ')
+                endereco = input('\nDigite um novo endereço: ')
                 alterar_cliente(endereco, 1, nome_antigo)
             elif (terceira_escolha == 3):
-                telefone = input('Digite um novo telefone: ')
+                telefone = input('\nDigite um novo telefone: ')
                 alterar_cliente(telefone, 2, nome_antigo)
 
             print(clientes)
@@ -45,9 +48,9 @@ def __main__():
 
             recarregar()
 
-
     elif(primeira_escolha == 2):
-        segunda_escolha = int(input('1 - Cadastrar Moto \n2 - Buscar por Moto \n3 - Atualizar dados da moto \n4 - Deletar Moto\nDigite o que deseja: '))
+        print(f'*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n' + 10 * ' ' + 'Motocicletas\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*')
+        segunda_escolha = int(input('\n1 - Cadastrar Moto \n2 - Buscar por Moto \n3 - Atualizar dados da moto \n4 - Deletar Moto\n\nDigite o que deseja: '))
         if (segunda_escolha == 1):
             modelo = input('Digite um modelo: ')
             marca = input('Digite um marca: ')
@@ -82,12 +85,18 @@ def __main__():
             modelo = input('Digite o modelo da moto que deseja deletar: ')
             deletar_moto(modelo)
             print(motos)
+            recarregar()
+
     elif(primeira_escolha == 3):
         print('Você escolheu vendas')
+
+    elif((primeira_escolha == 4) or (segunda_escolha == 5)):
+        carrega_mensagem_final()
     else:
-        print('Opção inválida')
+        print('Opção inválida, recomece o sistema.')
 
 
+            ############ CRUD DE CLIENTES ############
 
 def criar_cliente(nome, endereco, telefone, array):
     cliente = Cliente(nome, endereco, telefone)
@@ -95,6 +104,7 @@ def criar_cliente(nome, endereco, telefone, array):
 
 def buscar_cliente(nome):
     for cliente in clientes:
+        print(cliente)
         if(cliente[0] == nome):
             print(f'Nome do Cliente: {cliente[0]}\nEndereço do Cliente: {cliente[1]}\nTelefone do Cliente: {cliente[2]}')
 
@@ -114,6 +124,9 @@ def deletar_cliente(nome):
         if (cliente[0] == nome):
             clientes.remove(cliente)
 
+
+############ CRUD DE MOTOS ############
+
 def criar_moto(modelo, marca, ano, array):
     moto = Motocicletas(modelo, marca, ano)
     return array.append(([moto.modelo, moto.marca, moto.ano]))
@@ -132,6 +145,26 @@ def deletar_moto(modelo):
     for moto in motos:
         if (moto[0] == modelo):
             motos.remove(moto)
+
+
+############ MANIPULAÇÃO DE ARQUIVOS ############
+
+
+def carrega_mensagem_inicial():
+    arquivo = open("mensagem_inicial.txt", "r", encoding="utf-8")
+
+    for linha in arquivo:
+        print(linha)
+
+    arquivo.close()
+
+def carrega_mensagem_final():
+    arquivo = open("mensagem_final.txt", "r", encoding="utf-8")
+
+    for linha in arquivo:
+        print(linha)
+
+    arquivo.close()
 
 def recarregar():
     __main__()
