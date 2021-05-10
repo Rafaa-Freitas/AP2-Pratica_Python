26700141e2f5fe810fc1c07da9a1a853fe5a9dd9
Rafael Freitas <69989761+Rafaa-Freitas@users.noreply.github.com>
2021-05-10 18:09:17 -0300

    Trocando forma de permanecer dados
    Fazendo os dados permanecerem em arquivos

--- AP2-Prática_Python/concessionaria.py
+++ AP2-Prática_Python/concessionaria.py
@@ -1,9 +1,6 @@
 from cliente import Cliente
 from motocicletas import Motocicletas
 
-clientes = []
-motos = []
-
 def __main__():
     carrega_mensagem_inicial();
 
@@ -16,35 +13,34 @@ def __main__():
             nome = input('\nDigite um nome: ')
             endereco = input('\nDigite um endereço: ')
             telefone = input('\nDigite um telefone: ')
-            criar_cliente(nome, endereco, telefone, clientes)
-            print(f'\nCliente {nome} criado com sucesso!')
-
-            print(clientes)
+            criar_cliente(nome, endereco, telefone)
 
             recarregar()
         elif(segunda_escolha == 2):
             nome = input('Digite o nome do cliente para buscar os dados: ')
             buscar_cliente(nome)
-            recarregar()
+
+            recomecar = (input('Deseja recomeçar? (S/N) ')).upper()
+            if recomecar == 'S':
+                recarregar()
+            elif recomecar == 'N':
+                carrega_mensagem_final()
+            else:
+                print('Valor incorreto!')
+                recomecar = input('Deseja recomeçar? (S/N)')
+
         elif(segunda_escolha == 3):
             nome_antigo = input('Digite o nome do cliente que deve ser alterado: ')
-            terceira_escolha = int(input('1 - Nome, 2 - Endereço, 3 - Telefone\nO que deseja alterar: '))
-            if (terceira_escolha == 1):
-                nome = input('\nDigite um novo nome: ')
-                alterar_cliente(nome, 0, nome_antigo)
-            elif (terceira_escolha == 2):
-                endereco = input('\nDigite um novo endereço: ')
-                alterar_cliente(endereco, 1, nome_antigo)
-            elif (terceira_escolha == 3):
-                telefone = input('\nDigite um novo telefone: ')
-                alterar_cliente(telefone, 2, nome_antigo)
 
-            print(clientes)
+            nome_novo = input('\nDigite um novo nome: ')
+            endereco_novo = input('Digite um novo endereço: ')
+            telefone_novo = input('Digite um novo telefone: ')
+            alterar_cliente(nome_novo, endereco_novo, telefone_novo, nome_antigo)
+
             recarregar()
         elif(segunda_escolha == 4):
             nome = input('Digite o nome do cliente que deseja deletar: ')
             deletar_cliente(nome)
-            print(clientes)
 
             recarregar()
 
@@ -52,39 +48,38 @@ def __main__():
         print(f'*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n' + 10 * ' ' + 'Motocicletas\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*')
         segunda_escolha = int(input('\n1 - Cadastrar Moto \n2 - Buscar por Moto \n3 - Atualizar dados da moto \n4 - Deletar Moto\n\nDigite o que deseja: '))
         if (segunda_escolha == 1):
-            modelo = input('Digite um modelo: ')
+            modelo = input('\nDigite um modelo: ')
             marca = input('Digite um marca: ')
             ano = input('Digite um ano: ')
-            criar_moto(modelo, marca, ano, motos)
-            print(f'Moto {modelo} criada com sucesso!')
-
-            print(motos)
+            criar_moto(modelo, marca, ano)
 
             recarregar()
         elif (segunda_escolha == 2):
             modelo = input('Digite o modelo da moto para buscar os dados: ')
             buscar_moto(modelo)
-            recarregar()
+
+            recomecar = (input('Deseja recomeçar? (S/N) ')).upper()
+            if recomecar == 'S':
+                recarregar()
+            elif recomecar == 'N':
+                carrega_mensagem_final()
+            else:
+                print('Valor incorreto!')
+                recomecar = input('Deseja recomeçar? (S/N)')
 
         elif (segunda_escolha == 3):
             modelo_antigo = input('Digite o modelo da moto para alterar os dados: ')
-            terceira_escolha = int(input('1 - Modelo, 2 - Marca, 3 - Ano\nO que deseja alterar: '))
-            if (terceira_escolha == 1):
-                modelo = input('Digite um novo modelo: ')
-                alterar_moto(modelo, 0, modelo_antigo)
-            elif (terceira_escolha == 2):
-                marca = input('Digite uma nova marca: ')
-                alterar_moto(marca, 1, modelo_antigo)
-            elif (terceira_escolha == 3):
-                ano = input('Digite um novo ano: ')
-                alterar_moto(ano, 2, modelo_antigo)
+            modelo_novo = input('Digite um novo modelo: ')
+            marca_novo = input('Digite uma nova marca: ')
+            ano_novo = input('Digite um novo ano: ')
 
-            print(motos)
+            alterar_moto(modelo_novo, marca_novo, ano_novo, modelo_antigo)
+
             recarregar()
         elif (segunda_escolha == 4):
             modelo = input('Digite o modelo da moto que deseja deletar: ')
             deletar_moto(modelo)
-            print(motos)
+
             recarregar()
 
     elif(primeira_escolha == 3):
@@ -98,57 +93,90 @@ def __main__():
 
             ############ CRUD DE CLIENTES ############
 
-def criar_cliente(nome, endereco, telefone, array):
+def criar_cliente(nome, endereco, telefone):
     cliente = Cliente(nome, endereco, telefone)
-    return array.append([cliente.nome, cliente.endereco, cliente.telefone])
+
+    arquivo = open("clientes.txt", "a", encoding="utf-8")
+    arquivo.write(f'Nome: {cliente.nome}, Endereço: {cliente.endereco}, Telefone: {cliente.telefone}\n')
+    arquivo.close()
+
 
 def buscar_cliente(nome):
-    for cliente in clientes:
-        print(cliente)
-        if(cliente[0] == nome):
-            print(f'Nome do Cliente: {cliente[0]}\nEndereço do Cliente: {cliente[1]}\nTelefone do Cliente: {cliente[2]}')
+    arquivo = open("clientes.txt", "r", encoding="utf-8")
+    for linha in arquivo:
+        if nome in linha:
+            print(linha)
 
-def alterar_cliente(valor, index, valor_antigo):
-    for cliente in clientes:
-        if(valor_antigo == cliente[index]):
-            cliente[index] = valor
+def alterar_cliente(nome_novo, endereco_novo, telefone_novo, valor_antigo):
+    with open("clientes.txt", 'r', encoding="utf-8") as f:
+        texto = f.readlines()
+    for i in texto:
+        if valor_antigo in i:
+            index_linha = texto.index(i)
+            texto[index_linha] = (f'Nome: {nome_novo}, Endereço: {endereco_novo}, Telefone: {telefone_novo}\n')
+        else:
+            continue
+
+    with open("clientes.txt", 'w+', encoding="utf-8") as f:
+        f.writelines(texto)
 
 def deletar_cliente(nome):
-    for cliente in clientes:
-        if (cliente[0] == nome):
-            clientes.remove(cliente)
+    with open("clientes.txt", 'r', encoding="utf-8") as f:
+        texto = f.readlines()
+    for i in texto:
+        if nome in i:
+            index_linha = texto.index(i)
+            texto[index_linha] = ''
+        else:
+            continue
 
-
-def deletar_cliente(nome):
-    for cliente in clientes:
-        if (cliente[0] == nome):
-            clientes.remove(cliente)
+    with open("clientes.txt", 'w+', encoding="utf-8") as f:
+        f.writelines(texto)
 
 
 ############ CRUD DE MOTOS ############
 
-def criar_moto(modelo, marca, ano, array):
-    moto = Motocicletas(modelo, marca, ano)
-    return array.append(([moto.modelo, moto.marca, moto.ano]))
+def criar_moto(nome, endereco, telefone):
+    moto = Motocicletas(nome, endereco, telefone)
+
+    arquivo = open("motos.txt", "a", encoding="utf-8")
+    arquivo.write(f'Modelo: {moto.modelo}, Marca: {moto.marca}, Ano: {moto.ano}\n')
+    arquivo.close()
+
 
 def buscar_moto(modelo):
-    for moto in motos:
-        if(moto[0] == modelo):
-            print(f'Modelo da Moto: {moto[0]}\nMarca da Moto: {moto[1]}\nAno da Moto: {moto[2]}')
+    arquivo = open("motos.txt", "r", encoding="utf-8")
+    for linha in arquivo:
+        if modelo in linha:
+            print(linha)
 
-def alterar_moto(valor, index, valor_antigo):
-    for moto in motos:
-        if (valor_antigo == moto[index]):
-            moto[index] = valor
+def alterar_moto(modelo, marca, ano, valor_antigo):
+    with open("motos.txt", 'r', encoding="utf-8") as f:
+        texto = f.readlines()
+    for i in texto:
+        if valor_antigo in i:
+            index_linha = texto.index(i)
+            texto[index_linha] = (f'Nome: {modelo}, Endereço: {marca}, Telefone: {ano}\n')
+        else:
+            continue
+
+    with open("motos.txt", 'w+', encoding="utf-8") as f:
+        f.writelines(texto)
 
 def deletar_moto(modelo):
-    for moto in motos:
-        if (moto[0] == modelo):
-            motos.remove(moto)
+    with open("motos.txt", 'r', encoding="utf-8") as f:
+        texto = f.readlines()
+    for i in texto:
+        if modelo in i:
+            index_linha = texto.index(i)
+            texto[index_linha] = ''
+        else:
+            continue
 
+    with open("motos.txt", 'w+', encoding="utf-8") as f:
+        f.writelines(texto)
 
 ############ MANIPULAÇÃO DE ARQUIVOS ############
-
 
 def carrega_mensagem_inicial():
     arquivo = open("mensagem_inicial.txt", "r", encoding="utf-8")
